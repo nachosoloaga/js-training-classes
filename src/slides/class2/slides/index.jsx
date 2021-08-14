ca4f36a737ed110489474b87399654db32bbaa20
Julian Pasquale <julianagustin78@gmail.com>
2021-08-14 13:19:29 -0300

    Add basic parsing functions and mentions to Object methods

--- src/slides/class2/slides/index.jsx
+++ src/slides/class2/slides/index.jsx
@@ -1,3 +1,4 @@
+export { default as BuiltInFunctions } from './BuiltInFunctions';
 export { default as DomIntro } from './DomIntro';
 export { default as WindowAPI } from './WindowAPI';
 export { default as Debugger } from './Debugger';
