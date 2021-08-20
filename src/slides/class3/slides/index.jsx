2b2cdb2db434dc6e0bc9867d3bc13a0affe070ab
nachosoloaga <nachosoloaga@gmail.com>
2021-08-20 20:14:55 -0300

    CommonJS and ES6 modules slides

--- src/slides/class3/slides/index.jsx
+++ src/slides/class3/slides/index.jsx
@@ -3,3 +3,4 @@ export { default as ArrayMethods } from "./ArrayMethods";
 export { default as RestSpread } from "./RestSpread";
 export { default as Destructuring } from "./Destructuring";
 export { default as ConditionalChaining } from "./ConditionalChaining";
+export { default as Modules } from "./Modules";
